e866916c83fafabf3d45a5d0b4e534917cb6211c
Amarjeet <amarjeetofficial81@gmail.com>
2024-09-11 11:45:55 +0530

    bug fix in text styling buttons

--- public/script.js
+++ public/script.js
@@ -18,6 +18,34 @@ const italicTextInput = document.getElementById('italic')
 const alignTextInput = document.getElementById('align')
 //underline text input
 const underlineTextInput = document.getElementById('underline')
+//all checkboxes
+const checkboxes = document.querySelectorAll('.checkbox')
+//eventlistener on all checkboxes
+checkboxes.forEach(i => {
+    i.addEventListener('change', () => { console.log(i), styleCheckbox() })
+})
+//finction to change the style of checkbox
+function styleCheckbox(selectedText) {
+    if (selectedText) {
+        //for bold
+        selectedText.bold === true ? document.getElementById(`boldImg`).style.backgroundColor = '#b076f5' : document.getElementById(`boldImg`).style.backgroundColor = 'transparent'
+        //for italic
+        selectedText.italic === true ? document.getElementById(`italicImg`).style.backgroundColor = '#b076f5' : document.getElementById(`italicImg`).style.backgroundColor = 'transparent'
+        //for align
+        selectedText.align === true ? document.getElementById(`alignImg`).style.backgroundColor = '#b076f5' : document.getElementById(`alignImg`).style.backgroundColor = 'transparent'
+        //for underline
+        selectedText.underline === true ? document.getElementById(`underlineImg`).style.backgroundColor = '#b076f5' : document.getElementById(`underlineImg`).style.backgroundColor = 'transparent'
+    }
+    else {
+        checkboxes.forEach(i => {
+            if (i.checked) {
+                document.getElementById(`${i.id}Img`).style.backgroundColor = '#b076f5'
+            } else {
+                document.getElementById(`${i.id}Img`).style.backgroundColor = 'transparent'
+            }
+        })
+    }
+}
 
 //it will enable "Add" button when the input text length will be 1 or more then 1
 textInput.addEventListener('input', () => {
@@ -30,30 +58,30 @@ textInput.addEventListener('input', () => {
 })
 
 const texts = [
-    {
-        text: 'Drag me!',
-        x: 50,
-        y: 100,
-        fontStyle: 'Arial',
-        fontSize: 50,
-        bold: false,
-        italic: false,
-        align:false,
-        underline: false,
-        isDragging: false
-    },
-    {
-        text: "Move me too!",
-        x: 200,
-        y: 150,
-        fontStyle: 'Arial',
-        fontSize: 20,
-        bold: false,
-        italic: false,
-        align:false,
-        underline: false,
-        isDragging: false
-    }
+    // {
+    //     text: 'Drag me!',
+    //     x: 50,
+    //     y: 100,
+    //     fontStyle: 'Arial',
+    //     fontSize: 50,
+    //     bold: false,
+    //     italic: false,
+    //     align:false,
+    //     underline: false,
+    //     isDragging: false
+    // },
+    // {
+    //     text: "Move me too!",
+    //     x: 200,
+    //     y: 150,
+    //     fontStyle: 'Arial',
+    //     fontSize: 20,
+    //     bold: false,
+    //     italic: false,
+    //     align:false,
+    //     underline: false,
+    //     isDragging: false
+    // }
 ]
 
 //adding text on canvas
@@ -74,23 +102,32 @@ addBtn.addEventListener('click', () => {
     }
     ctx.font = `${italic} ${bold} ${fontSize}px ${fontStyle}`
     //appending text into a texts array
+
+    //parsing bold and italic to save the text
+    //for bold
+    if (bold === 'bold') {
+        bold = true
+    } else {
+        bold = false
+    }
+    //for italic
+    if (italic === 'italic') {
+        italic = true
+    } else {
+        italic = false
+    }
     let newText = {
         text: text,
         x: x,
         y: y,
         fontStyle: fontStyle,
         fontSize: fontSize,
-        bold: function () {
-            return boldTextInput.checked === true ? true : false
-        },
-        italic: function () {
-            return italicTextInput.checked === true ? true : false
-        },
-        align:true,
+        bold: bold,
+        italic: italic,
+        align: true,
         underline: false,
         isDragging: false
     }
-    console.log(newText.bold())
     texts.push(newText)
     ctx.fillText(text, x, y)
     textInput.value = ''
@@ -102,7 +139,9 @@ addBtn.addEventListener('click', () => {
 function drawTexts() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     texts.forEach(textObject => {
-        ctx.font = `${textObject.fontSize}px ${textObject.fontStyle}`;
+        let isBold = textObject.bold === true ? 'bold' : 'normal'
+        let isItalic = textObject.italic === true ? 'italic' : 'normal'
+        ctx.font = `${isItalic} ${isBold} ${textObject.fontSize}px ${textObject.fontStyle}`;
         ctx.fillStyle = textObject.color;
         ctx.fillText(textObject.text, textObject.x, textObject.y);
     });
@@ -133,10 +172,16 @@ canvas.addEventListener('dblclick', (e) => {
     let mouseX = e.offsetX
     let mouseY = e.offsetY
     texts.forEach(textObject => {
-        let textWidth = ctx.measureText(textObject.text).width
-        let textHeight = parseInt(`${textObject.fontSize}px ${textObject.fontStyle}`, 10)
         if (checkMouseOver(mouseX, mouseY, textObject)) {
             selectedText = textObject
+            //updateing the input feild according to this object
+            fontStyleInput.value = selectedText.fontStyle
+            fontSizeInput.value = selectedText.fontSize
+            boldTextInput.checked = selectedText.bold
+            italicTextInput.checked = selectedText.italic
+            alignTextInput.checked = selectedText.align
+            underlineTextInput.checked = selectedText.underline
+            styleCheckbox(selectedText)
         }
     })
 })
